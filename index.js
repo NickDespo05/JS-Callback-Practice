7acfb99ed53a80e4710b390c8ccba5afae81ff5c
Nick D'Esposito <Nicholas D'Esposito>
2022-05-10 20:09:08 -0400

    Done

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+const character = newImage("assets/green-character/static.gif");
 
+move(character).withArrowKeys(100, 250);
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
+
+/**
+ * Notes before going on:
+ * had an error in the console something with chrome but it did nothing to the website so I just left it be
+ * for some reason the gif is not properly rendering on the site but the picture of the character itself does change
+ */
--- move.js
+++ move.js
@@ -1,12 +1,75 @@
 function move(element) {
-    element.style.position = 'fixed'
+    element.style.position = "fixed";
 
     function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+        element.style.left = left + "px";
+        element.style.bottom = bottom + "px";
     }
+    function moveWithArrowKeys(left, bottom) {
+        let direction = null;
+        let x = left;
+        let y = bottom;
+        element.style.left = left + "px";
+        element.style.bottom = bottom + "px";
+        function moveCharacter() {
+            if (direction === null) {
+                character.src = "assets/green-character/static.gif";
+            }
+            if (direction === "west") {
+                move(character).to((x -= 1));
+                character.src = "assets/green-character/west.gif";
+            }
+            if (direction === "east") {
+                move(character).to((x += 1));
+                character.src = "assets/green-character/east.gif";
+            }
+            if (direction === "north") {
+                move(character).to((y += 1));
+                character.src = "assets/green-character/north.gif";
+            }
+            if (direction === "south") {
+                move(character).to((y -= 1));
+                character.src = "assets/green-character/south.gif";
+            }
+            element.style.left = x + "px";
+            element.style.bottom = y + "px";
+        }
+        setInterval(moveCharacter, 1);
 
-    return {
-        to: moveToCoordinates
+        document.addEventListener("keydown", function (e) {
+            if (e.repeat) return;
+
+            if (e.key === "ArrowLeft") {
+                direction = "west";
+            }
+            if (e.key === "ArrowUp") {
+                direction = "north";
+            }
+            if (e.key === "ArrowRight") {
+                direction = "east";
+            }
+            if (e.key === "ArrowDown") {
+                direction = "south";
+            }
+        });
+        // if (direction === null) {
+        //     character.src = "assets/green-character/static.gif";
+        // } else if (direction === "north") {
+        //     character.src = "assets/green-character/north.gif";
+        // } else if (direction === "south") {
+        //     character.src = "assets/green-character/south.gif";
+        // } else if (direction === "east") {
+        //     character.src = "assets/green-character/east.gif";
+        // } else if (direction === "west") {
+        //     character.src = "assets/green-character/west.gif";
+        // }
+
+        document.addEventListener("keyup", function (e) {
+            direction = null;
+        });
     }
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys,
+    };
 }
